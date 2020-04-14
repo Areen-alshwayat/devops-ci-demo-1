ba79719b626c29330aa6c6300f9fc4ff5ff663f5
Areen-alshwayat <61476000+Areen-alshwayat@users.noreply.github.com>
2020-04-14 22:22:57 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,17 +9,3 @@ echo "The logged user is: ${USER}."
 echo "CURRENT WORKING DIERCTORY IS: ${PWD} ."
 
 
-server=$HOSTNAME
-name=$USER
-timestamp=$(date +%Y-%m-%d_%H-%M-%S)
-DIR=$(pwd)
-branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-
-sed  "s/{SERVER_NAME}/$server/g" web/indx.txt  |sed  "s/{USER}/$name/g" >indx.txt
-sed -i "s/{TIMESTAMP}/$timestamp/g" indx.txt
-sed -i "s?{GIT_BRANCH}?$branch?g" indx.txt
-sed -i "s?{GIT_REPO}?$GIT_REPO?g" indx.txt
-sed -i "s/{AZURE_VARIABLE}/$AZURE_VARIABLE/g" indx.txt
-
-
-sed  "s~{PWD}~$DIR~g" indx.txt>web/index.html
