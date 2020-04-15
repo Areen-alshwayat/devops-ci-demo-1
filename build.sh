e648ee7495fe01cc82b6fdb4210452107af1f8a8
Areen-alshwayat <61476000+Areen-alshwayat@users.noreply.github.com>
2020-04-15 15:17:53 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -14,9 +14,12 @@ timestamp=$( date +%T )
 dir=$(PWD)
 branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
-
+sed  "s/{SERVER_NAME}/$server/g" web/index.txt  |sed  "s/{USER}/$name/g" >index.txt
 sed -i "s/{TIMESTAMP}/$timestamp/g" index.html
 sed -i "s/{SERVER_NAME}/$server/g"  index.html
 sed -i "s/{AZURE_VARIABLE}/$AZURE_VARIABLE/g" index.txt
+
+
+sed  "s~{PWD}~$dir~g" >web/index.html
 
 
