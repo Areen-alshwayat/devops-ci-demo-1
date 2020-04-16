0a5bc252cc42664e6d7fd43984b5827938e7523e
Areen-alshwayat <61476000+Areen-alshwayat@users.noreply.github.com>
2020-04-16 23:49:05 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,14 +8,17 @@ echo "The time is $(date +%Y-%m-%d_%H-%M-%S)."
 echo "The logged user is: ${USER}."
 echo "CURRENT WORKING DIERCTORY IS: ${PWD} ."
 
+sed -i "s/{/\${g" web/index.html
+
+
+
 server=$HOSTNAME
 name=$USER
 timestamp=$(date +%Y-%m-%d_%H-%M-%S)
 dir=$(pwd)
-branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
 sed  "s/{SERVER_NAME}/$server/g" web/index.txt  |sed  "s/{USER}/$name/g" >index.txt
-sed -i  "s/{TIMESTAMP}/$timestamp/g" index.html
-sed -i  "s/{SERVER_NAME}/$server/g"  index.html
+
+
 
 
