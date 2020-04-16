6d9b2d5e842c73e084d2141be585606cc4a1eb57
Areen-alshwayat <61476000+Areen-alshwayat@users.noreply.github.com>
2020-04-17 00:00:30 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,23 +1,13 @@
 #! /bin/bash
 
 # Echo this is the build script
-echo "Running the build script..."
-
-echo " I am $HOSTNAME."
-echo "The time is $(date +%Y-%m-%d_%H-%M-%S)."
-echo "The logged user is: ${USER}."
-echo "CURRENT WORKING DIERCTORY IS: ${PWD} ."
-
-sed -i "s/{/\${g" web/index.html
 
 
-
-server=$HOSTNAME
-name=$USER
-timestamp=$(date +%Y-%m-%d_%H-%M-%S)
-dir=$(pwd)
-
-sed  "s/{SERVER_NAME}/$server/g" web/index.txt  |sed  "s/{USER}/$name/g" >index.txt
+export TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
+sed -i "s/{/\${/g" web/index.html
+envsubst '$GIT_REPO $GIT_BRANCH $AZURE_VARIABLE > web/newIndex.html <  web/index.html
+cp web/newIndex.html web/index.html
+ 
 
 
 
