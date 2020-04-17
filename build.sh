a4a8d335c11b663cd032043082124a8f16ac5ed6
Areen-alshwayat <61476000+Areen-alshwayat@users.noreply.github.com>
2020-04-17 13:57:57 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 
 # Echo this is the build script
 
+  sed -i "s/{/\${/g" web/index.html
 
-envsubst '$GIT_REPO $GIT_BRANCH $AZURE_VARIABLE' <  web/index.html
+envsubst '$GIT_REPO $GIT_BRANCH $AZURE_VARIABLE' <web/index.html
 
 
