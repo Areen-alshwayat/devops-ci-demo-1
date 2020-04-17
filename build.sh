d9a841989130091f2ac42a7e2c867b8d92e4e427
Areen-alshwayat <61476000+Areen-alshwayat@users.noreply.github.com>
2020-04-17 14:07:50 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,10 +5,8 @@
 
   sed -i "s/{/\${/g" web/index.html
 
-envsubst '$GIT_REPO $GIT_BRANCH $AZURE_VARIABLE' <web/index.html
-
-sed -i "s?{GIT_BRANCH}?$branch?g" index.txt
-sed -i "s?{GIT_REPO}?$GIT_REPO?g" index.txt
-sed -i "s/{AZURE_VARIABLE}/$AZURE_VARIABLE/g" index.txt
+sed -i "s?{GIT_BRANCH}?$branch?g" .web/index.txt
+sed -i "s?{GIT_REPO}?$GIT_REPO?g" .web/index.txt
+sed -i "s/{AZURE_VARIABLE}/$AZURE_VARIABLE/g" .web/index.txt
 
 
