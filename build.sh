8553e50c4f109968dcbef0fa7b7b693543bd1564
Areen-alshwayat <61476000+Areen-alshwayat@users.noreply.github.com>
2020-04-17 14:27:21 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,11 @@
 
 # Echo this is the build script
 
+echo "Running the Build Sprict"
+echo "I am $HOSTNAME ."
+echo "the Time is $(date +%Y-%m-%d_%H-%M-%S)"
+echo "the loged in user is $USER "
 
+
+  sed -i "s/{/\${/g" web/index.html
 envsubst '$GIT_REPO $GIT_BRANCH $AZURE_VARIABLE'>web/index.html
